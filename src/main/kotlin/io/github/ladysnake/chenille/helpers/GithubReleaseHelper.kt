b81c6e2b306613b305c7c02937ba37148254b1f9
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-07-23 00:48:37 +0200

    Configure GitHub release plugin from extension URL

--- src/main/kotlin/io/github/ladysnake/chenille/helpers/GithubReleaseHelper.kt
+++ src/main/kotlin/io/github/ladysnake/chenille/helpers/GithubReleaseHelper.kt
@@ -21,8 +21,12 @@ import com.github.breadmoirai.githubreleaseplugin.GithubReleaseExtension
 import io.github.ladysnake.chenille.ChenilleProject
 import io.github.ladysnake.chenille.api.PublishingConfiguration
 import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.the
 
 internal object GithubReleaseHelper {
+
+    val repositoryRegex = Regex("^(?<host>(?:git@|https://)[\\w.@]+)[/:](?<owner>[\\w_-]+)/(?<repo>[\\w_-]+)(?:.git)?/?$")
+
     fun configureDefaults(project: ChenilleProject, cfg: PublishingConfiguration) {
         project.pluginManager.apply("com.github.breadmoirai.github-release")
 
@@ -30,6 +34,24 @@ internal object GithubReleaseHelper {
             setToken(project.providers.gradleProperty("github_api_key"))
             // default owner: last component of maven group
             // default repo: name of the project
+            project.extension.github
+                ?.let { repositoryRegex.matchEntire(it.toString()) }
+                ?.let { matchResult ->
+                    val host = matchResult.groups["owner"]!!.value
+                    val repoOwner = matchResult.groups["owner"]!!.value
+                    val repoName = matchResult.groups["repo"]!!.value
+
+                    project.logger.debug("Found Host: {} Owner: {} Repo: {}", host, repoOwner, repoName)
+
+                    if(host.contains("github.com")) {
+                        project.logger.lifecycle("Unable to configure github release: Repository host is not GitHub! (found ${host})")
+                        return@configure
+                    }
+
+                    owner = repoOwner
+                    repo = repoName
+                }
+
             tagName = project.version.toString()
             targetCommitish = project.git?.currentBranch()
             body = project.changelog.call().toString()
